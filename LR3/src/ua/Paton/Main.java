def7141e1cf6fb6681f6c36d2ab2fbf16dbb2f7d
Horshenia <leib-horshenia@ukr.net>
2022-09-26 12:04:26 +0300

    Створення

---
+++ LR3/src/ua/Paton/Main.java
@@ -0,0 +1,18 @@
+package ua.Paton;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		short s1 = 99, s2 = 99;
+		float f1, f2;
+		byte b = -10;
+		@SuppressWarnings("unused")
+		boolean q = true;
+		s1 = (short) Math.pow(b, 2);
+		s2 = (short) ((Math.random() * (10 - 0)) + 0);
+		f2 = (float) ((Math.random() * (9999 - 1)) + 1);
+		f1 = (float) f2/(s1*s2);
+		System.out.println("s1 = "+s1+"\ns2 = "+s2+"\nf1 = "+f1+"\nf2 = "+f2);
+		System.out.println("y = "+Math.pow(Math.tan(f1), s2)/Math.log(s1));
+	}
+}
